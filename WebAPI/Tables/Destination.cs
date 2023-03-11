46675446376b05954e9c6019ece038e529af19df
OndrejXXX <pachaondrej@sssvt.cz>
2023-03-11 17:15:56 +0100

    DestinationCreate

--- WebAPI/Tables/Destination.cs
+++ WebAPI/Tables/Destination.cs
@@ -1,6 +1,13 @@
-﻿namespace WebAPI.Tables
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Tables
 {
+    [Table("Destination")]
     public class Destination
     {
+        public int ID { get; set; }
+        public int Config { get; set; }
+        public string DestinationPath { get; set; }
+
     }
 }
