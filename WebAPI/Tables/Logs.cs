8768a1a354da04ac26071b11df12f85b27eca0b2
ArtemUnreal71 <artemonych628@gmail.com>
2023-03-11 17:11:59 +0100

    Create Logs.cs

---
+++ WebAPI/Tables/Logs.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WebAPI.Tables
+{
+    [Table("Logs")]
+    public class Logs
+    {
+        public int ID { get; set; }
+
+        public int ComputerID { get; set; }
+
+        public int ConfigID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Errors { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
