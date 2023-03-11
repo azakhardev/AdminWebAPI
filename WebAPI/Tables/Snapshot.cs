83cae71155040cb3e61c3da41325292f3bb6eefa
ArtemUnreal71 <artemonych628@gmail.com>
2023-03-11 17:30:45 +0100

    Create Snapshot.cs

---
+++ WebAPI/Tables/Snapshot.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace WebAPI.Tables
+{
+    [Table("Snapshot")]
+    public class Snapshot
+    {
+        public int ID { get; set; }
+
+        public int ConfigID { get; set; }
+
+        public int SnapshotVersion { get; set; }
+
+        public int Size { get; set; }
+
+        public int MaxPackageAmount { get; set; }
+
+        public int MaxPackageSize { get; set; }
+
+    }
+}
