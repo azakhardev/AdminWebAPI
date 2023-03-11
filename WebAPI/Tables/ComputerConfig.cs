0aa63df4bd164d60d30b7654ddd4250b8c4eabf7
OndrejXXX <pachaondrej@sssvt.cz>
2023-03-11 17:23:49 +0100

    Create ComputerConfig.cs

---
+++ WebAPI/Tables/ComputerConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Tables
+{
+    [Table("ComputerConfig")]
+    public class ComputerConfig
+    {
+        public int ID { get; set; }
+        public int ConfigID { get; set; }  
+
+    }
+}
