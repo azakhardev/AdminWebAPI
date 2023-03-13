87dbec613dc710a92302bdc05abe05d36a974a5c
ArtemUnreal71 <artemonych628@gmail.com>
2023-03-13 20:44:38 +0100

    ComputersCtr methods

--- WebAPI/Controllers/ComputersCtr.cs
+++ WebAPI/Controllers/ComputersCtr.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Mysqlx.Crud;
+using System.Collections.Generic;
+using System.Globalization;
+using WebAPI.Tables;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers
@@ -8,36 +12,59 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ComputersCtr : ControllerBase
     {
+        BackupDatabase dbBackup = new BackupDatabase();
         // GET: api/<Computers>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<tbComputers> Get()
         {
-            return new string[] { "value1", "value2" };
+            return dbBackup.Computers;
         }
 
         // GET api/<Computers>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public tbComputers Get(int id)
         {
-            return "value";
+            return dbBackup.Computers.Find(id);
         }
 
         // POST api/<Computers>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post(string computerName, string backupStatus, string computerStatus, string description, DateTime lastBackup)
         {
+            dbBackup.Computers.Add(new tbComputers()
+            {
+                ComputerName = computerName,
+                BackupStatus = backupStatus,
+                ComputerStatus = computerStatus,
+                Description = description,
+                LastBackup = lastBackup
+            });
+
+            dbBackup.SaveChanges();
         }
 
         // PUT api/<Computers>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, string newComputerName, string newBackupStatus, string newComputerStatus, string newDescription)
         {
+            // = dbBackup.Computers.Find(id).ComputerName
+            tbComputers updatedComputer = dbBackup.Computers.Find(id);
+
+            updatedComputer.ComputerName = newComputerName;
+            updatedComputer.BackupStatus = newBackupStatus;
+            updatedComputer.ComputerStatus = newComputerStatus;
+            updatedComputer.Description = newDescription;
+            //updatedComputer.LastBackup = newLastBackup;
+
+            dbBackup.SaveChanges();
         }
 
         // DELETE api/<Computers>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            dbBackup.Computers.Remove(dbBackup.Computers.Find(id));
+            dbBackup.SaveChanges();
         }
     }
 }
