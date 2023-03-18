d3806db95cf93d95f56c8b1dd75ff1c051a8e27f
OndrejXXX <pachaondrej@sssvt.cz>
2023-03-18 12:41:39 +0100

    Groups

---
+++ WebAPI/FormatCheck/GroupCheck.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI.FormatCheck
+{
+    public class GroupCheck
+    {
+        public void CheckAll()
+        {
+            
+        }
+
+        public void GroupNameCheck(string groupName)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid group name");
+        }
+
+        public void ComputerIDCheck(string computerID)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid computer ID");
+        }
+    }
+}
