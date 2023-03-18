1654f65a67941ac53ef4301c799a8a6881417a6d
OndrejXXX <pachaondrej@sssvt.cz>
2023-03-18 12:31:39 +0100

    Create ConfigCheck.cs

---
+++ WebAPI/FormatCheck/ConfigCheck.cs
@@ -0,0 +1,43 @@
+﻿namespace WebAPI.FormatCheck
+{
+    public class ConfigCheck
+    {
+        public void CheckAll()
+        {
+
+        }
+
+
+        public void ConfigNameCheck(string configName)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid config name");
+        }
+
+
+        public void AlgorithmCheck(string algorithm)
+        {
+            if (true)
+               return;
+            throw new FormatException("Invalid algorithm");
+        }
+
+        public void MaxPackageAmountCheck(int maxPackageAmount)
+        {
+            if (true)            
+                return;
+            throw new FormatException("Invalid max package amount");
+        }
+
+        public void MaxPackageSizeCheck(int maxPackageSize)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid max package size");
+        }
+
+
+
+    }
+}
