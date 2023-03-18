03db254bcb33493fda2f5ff2dad0953be5ab0e11
OndrejXXX <pachaondrej@sssvt.cz>
2023-03-18 12:46:41 +0100

    Groups

---
+++ WebAPI/FormatCheck/LogCheck.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI.FormatCheck
+{
+    public class LogCheck
+    {
+        public void CheckAll()
+        {
+
+        }
+
+        public void ComputersConfigsIDCheck(int computerConfigID)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid computer config ID");
+        }
+
+        public void DateCheck(DateTime Date)
+        {
+            if (true)
+                return;
+            throw new FormatException("Invalid date");
+        }
+    }
+}
