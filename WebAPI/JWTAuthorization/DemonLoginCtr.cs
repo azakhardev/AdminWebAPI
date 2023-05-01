4e6618bd3ee4579647f5dcb80975ad350f115a4d
ArtemUnreal71 <artemonych628@gmail.com>
2023-05-01 11:06:49 +0200

    Pridano overeni pro demona

---
+++ WebAPI/JWTAuthorization/DemonLoginCtr.cs
@@ -0,0 +1,44 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Tables.Help_Tables;
+using WebAPI.Tables;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.JWTAuthorization
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemonLoginCtr : ControllerBase
+    {
+        BackupDatabase dbBackup = new BackupDatabase();
+
+        [HttpPost]
+        public ActionResult<string> Post(ComputerForLogin login)
+        {
+            try
+            {
+                ComputersTb computer = dbBackup.Computers.Where(x => x.ID == login.ID).First();
+
+                if (login.ComputerStatus != "Blocked")
+                {
+                    string token = JwtBuilder.Create()
+                    .WithAlgorithm(new HMACSHA256Algorithm())
+                    .WithSecret("super-secret-bar")
+                    .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                    .AddClaim("computer", computer.ComputerName)
+                    .AddClaim("status", computer.ComputerStatus)
+                    .Encode();
+
+                    return Ok(new { token = token });
+                }
+                return Unauthorized(new { message = "Computer is blocekd. Access denied." });
+            }
+            catch
+            {
+                return Unauthorized(new { message = "Wrong computer ID." });
+            }
+        }
+    }
+}
---
+++ WebAPI/Tables/Help Tables/ComputerForLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Tables.Help_Tables
+{
+    public class ComputerForLogin
+    {
+        public int ID { get; set; }
+
+        public string ComputerStatus { get; set; }
+    }
+}
