ce5522e00054af5408aaf126ca0c00ba78392bcc
ArtemUnreal71 <artemonych628@gmail.com>
2023-05-21 22:42:58 +0200

    odstraneni overeni cronu

--- WebAPI/FormatCheck/ConfigCheck.cs
+++ WebAPI/FormatCheck/ConfigCheck.cs
@@ -45,7 +45,7 @@ namespace WebAPI.FormatCheck
 
         public void ScheduleCheck(ConfigsTb config)
         {
-            if (Regex.IsMatch(config.Schedule, @"^(*|\d{1,2}|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2},\d{1,2}) (*|\d{1,2}|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2},\d{1,2}) (*|\d{1,2}|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2},\d{1,2}) (*|\d{1,2}|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2},\d{1,2}) (*|\d{1,2}|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2},\d{1,2})$"))
+            if (Regex.IsMatch(config.Schedule, @""))
                 return;
             throw new FormatException("Invalid schedule");
         }
